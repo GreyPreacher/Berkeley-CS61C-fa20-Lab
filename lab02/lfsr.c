0e7adf001c0118db9f5ed6ba1b3c3c8fcc4e6412
GreyPreacher <zhangxiaoting320@gmail.com>
2022-12-09 11:47:39 +0800

    complete lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,18 @@
 #include <string.h>
 #include "lfsr.h"
 
+int next_msb(uint16_t *reg){
+    int x = 0;
+    x ^= ((*reg)&1);
+    x ^= (((*reg)>>2)&1);
+    x ^= (((*reg)>>3)&1);
+    x ^= (((*reg)>>5)&1);
+    return x;
+}
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    int msb = next_msb(reg);
+    (*reg) >>= 1;
+    (*reg) |= (msb<<15);
 }
 
